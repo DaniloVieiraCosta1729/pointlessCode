87df27b1bb1b6dabf553853648ac7dba1499baa9
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-12-07 15:39:58 -0300

    Main file, not implemented yet.

---
+++ EP001/src/Stones-belarusTST2019.c
@@ -0,0 +1,14 @@
+/*
+    <PROBLEM>
+    (Belarus/TST- 2019) 1019 stones are placed into two non-empty boxes. Each second Alex chooses a box with an even amount of stones and shifts half of these stones into another box. Prove that for each k, 1 ≤ k ≤ 1018, at some moment there will be a box with exactly k stones.
+
+    Solution 01: Brute Force.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
